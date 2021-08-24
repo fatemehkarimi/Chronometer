de671bd5b4085646734f4a83d2eb1b619bd21bc1
Ghasem Ramezani <g1999ramezani@gmail.com>
2021-08-24 20:45:57 +0430

    - Memory leak detected by ASAN was resolved in the main.cpp.
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,22 @@
 #include <Chronometer/TimeController.h>
 #include <Chronometer/Timer.h>
 
-using namespace std;
-
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    MainWindow* window = new MainWindow(700, 700, "Chronometer");
+    MainWindow window(700, 700, "Chronometer");
+
     // Timer
-    Timer* t_timer = new Timer();
-    Controller* time_controller = new TimeController(t_timer);
-    window->registerWindow(time_controller->getView(), "Timer");
+    Timer t_timer;
+    TimeController time_controller(&t_timer);
+    window.registerWindow(time_controller.getView(), "Timer");
 
     // Chronometer
-    Timer* c_timer = new Timer();
-    Controller* chrono_controller = new ChronoController(c_timer);
-    window->registerWindow(chrono_controller->getView(), "Chronometer");
+    Timer c_timer;
+    ChronoController chrono_controller(&c_timer);
 
-    window->show();
+    window.registerWindow(chrono_controller.getView(), "Chronometer");
+    window.show();
+
     return app.exec();
 }
